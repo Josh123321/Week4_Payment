0ba50d8fc9d07f62b9eb0379dfd5b34a4372665f
Josh Wu <josh.wu@tradevan.com.tw>
2019-08-12 10:53:02 +0800

    Updates

---
+++ precache-manifest.270a6d955b030dab3c4e785aeeb9af6b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968d351c5b37205c836092444b57b854",
+    "url": "/Week4_payment/index.html"
+  },
+  {
+    "revision": "53f1c1947bfa9c17b861",
+    "url": "/Week4_payment/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9bcafc44bc0bc21c1572",
+    "url": "/Week4_payment/static/css/main.56c1bd40.chunk.css"
+  },
+  {
+    "revision": "53f1c1947bfa9c17b861",
+    "url": "/Week4_payment/static/js/2.5c180a28.chunk.js"
+  },
+  {
+    "revision": "9bcafc44bc0bc21c1572",
+    "url": "/Week4_payment/static/js/main.ac6e2ddd.chunk.js"
+  },
+  {
+    "revision": "795476228b95e7b03fcc",
+    "url": "/Week4_payment/static/js/runtime~main.87ed117a.js"
+  },
+  {
+    "revision": "dc09e2e2a98b1f3b97177373a0c7529c",
+    "url": "/Week4_payment/static/media/icon_arrow_right.dc09e2e2.svg"
+  },
+  {
+    "revision": "4de835edd86280db95a28b3ed2588f0a",
+    "url": "/Week4_payment/static/media/icon_atm.4de835ed.svg"
+  },
+  {
+    "revision": "f20b133e7c3b2c66afae4f26109d4945",
+    "url": "/Week4_payment/static/media/icon_card.f20b133e.svg"
+  },
+  {
+    "revision": "2e35ffda3d0763afcdae3f6147ad2633",
+    "url": "/Week4_payment/static/media/icon_confirm.2e35ffda.svg"
+  },
+  {
+    "revision": "9a3b2d961adf60d623faa93c67143676",
+    "url": "/Week4_payment/static/media/icon_confirm_normal.9a3b2d96.svg"
+  },
+  {
+    "revision": "b2071eb0dd76b3fd68a4fd9458cfd24e",
+    "url": "/Week4_payment/static/media/icon_notice.b2071eb0.svg"
+  },
+  {
+    "revision": "1bc3ee175d1a6234db51ff2665707f72",
+    "url": "/Week4_payment/static/media/icon_step2.1bc3ee17.svg"
+  },
+  {
+    "revision": "2b60e60bc54fa43c87a7c4e66e81c371",
+    "url": "/Week4_payment/static/media/icon_step3.2b60e60b.svg"
+  },
+  {
+    "revision": "3a4a0ec24ab5061ff2bac3e539b00740",
+    "url": "/Week4_payment/static/media/icon_step3_active.3a4a0ec2.svg"
+  },
+  {
+    "revision": "f3709b1f68d5ea8c89419b1a8a2649a1",
+    "url": "/Week4_payment/static/media/icon_step_done.f3709b1f.svg"
+  },
+  {
+    "revision": "c84e776b557443f544edbf14c58b05d1",
+    "url": "/Week4_payment/static/media/icon_store.c84e776b.svg"
+  },
+  {
+    "revision": "f6e877e3a997c32df73c621371f4f3eb",
+    "url": "/Week4_payment/static/media/line_pay.f6e877e3.svg"
+  },
+  {
+    "revision": "4cd422d7c3672f293d6534013e2a8ec1",
+    "url": "/Week4_payment/static/media/logo.4cd422d7.svg"
+  },
+  {
+    "revision": "1f53e6c70760cbd892254dcdbc6df35f",
+    "url": "/Week4_payment/static/media/unionpay.1f53e6c7.png"
+  }
+]);
